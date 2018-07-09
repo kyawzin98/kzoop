5f0a642c570fabffa1b14bf38d115ab564d9fda3
Kyaw Zin <immortalityskill2398@gmail.com>
2018-07-09 20:11:53 +0630

    Add OOP vedio homework

--- ShopProductWriter.php
+++
@@ -1,8 +0,0 @@
-<?php
-class ShopProductWriter {
-    public function write(ShopProduct $shopProduct ) {
-        $str  = "{$shopProduct->title}: " .
-            $shopProduct->getProducer() .
-            " ({$shopProduct->price})\n";
-        print $str; }
-}
