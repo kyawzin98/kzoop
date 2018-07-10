a486c56ada8e9fbb9fc098d72a9323ba49891a33
Kyaw Zin <immortalityskill2398@gmail.com>
2018-07-10 08:16:50 +0630

    Finish 02 Getter and Setter Vedio File

---
+++ 1/Person.php
@@ -0,0 +1,32 @@
+<?php
+
+class Person
+{
+    public $name;
+
+    private $age;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function SetAge($age)
+    {
+        if ($age < 18) {
+            throw new Exception('Person is not old Enough');
+        }
+        $this->age = $age;
+    }
+
+    public function GetAge()
+    {
+        return $this->age;
+    }
+}
+
+$person = new Person('Kyaw Zin');
+$person->SetAge(19);
+//$person->age=4;
+//var_dump($person->age);
+var_dump($person->GetAge());
